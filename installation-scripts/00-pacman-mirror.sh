7db12cfd747d721c72b7f2dda4c475ad4c5dab1f
Markus Pesch <markus.pesch@dedalusgroup.com>
2020-10-11 18:45:40 +0200

    fix: aur repos

--- installation-scripts/00-pacman-mirror.sh
+++ installation-scripts/00-pacman-mirror.sh
@@ -14,11 +14,11 @@ pacman-key --recv-keys 9B146D11A9ED6CA7E279EB1A852BCC170D81A982
 pacman-key --lsign 9B146D11A9ED6CA7E279EB1A852BCC170D81A982
 
 # Add additional pacman mirrors
-cat >> /etc/pacman.conf << 'EOF'
+cat >> /etc/pacman.conf <<EOF
 
-[cs_any]
+[any]
 Server = https://aur.cryptic.systems/any/
 
-[cs_x86_64]
+[x86_64]
 Server = https://aur.cryptic.systems/x86_64/
 EOF
